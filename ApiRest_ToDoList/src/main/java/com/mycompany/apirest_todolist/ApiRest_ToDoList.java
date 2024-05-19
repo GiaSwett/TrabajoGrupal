8089153022bdb89a017823ac2893b435c297292f
GiaSwett <itatiswett@gmail.com>
2024-05-19 15:16:08 -0500

    Add files via upload

---
+++ ApiRest_ToDoList/src/main/java/com/mycompany/apirest_todolist/ApiRest_ToDoList.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apirest_todolist;
+
+import Vista.ToDoList;
+
+/**
+ *
+ * @author Itati
+ */
+public class ApiRest_ToDoList {
+
+    public static void main(String[] args) {
+        
+        ToDoList principal = new ToDoList();
+        principal.setVisible(true);
+        
+        //APAREZCA EN EL CENTRO DE LA PANTALLA
+        principal.setLocationRelativeTo(null);
+        
+    }
+}
---
+++ ApiRest_ToDoList/target/generated-sources/annotations/Logica/ListaTareas_.java
@@ -0,0 +1,16 @@
+package Logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-03-04T21:25:03", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(ListaTareas.class)
+public class ListaTareas_ { 
+
+    public static volatile SingularAttribute<ListaTareas, String> descripcion;
+    public static volatile SingularAttribute<ListaTareas, String> estado;
+    public static volatile SingularAttribute<ListaTareas, String> titulo;
+    public static volatile SingularAttribute<ListaTareas, Integer> id;
+
+}
